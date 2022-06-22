0cda7ef83fa1c320a687940da1de6e619d4a6eac
MarcosChagas <outrochagas@gmail.com>
2022-06-22 11:15:14 -0400

    javascript aprendendo

---
+++ estudos/letscode/modulo01/aula05/script04.js
@@ -0,0 +1,12 @@
+let sinal = 'verde'
+switch(sinal){
+    case 'amarelo' :
+        console.log ('amarelo, não siga');
+        break
+    case 'verde' :
+        console.log ('verde, siga !');
+        break
+    case 'vermelho' :
+        console.log ('vermelho, pare !');
+        break;
+}
